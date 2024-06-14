d2fbbdd439dad5b9494c6d0c75777419fe9ace49
Sarang S <email.sarangs@gmail.com>
2024-06-14 15:05:48 +0530

    fix

--- price_forecast.py
+++ price_forecast.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
 
         print(data.head())
 
-        train_data, test_data = train_test_split(data, test_size=0.1, shuffle=False)
+        train_data, test_data = train_test_split(data, test_size=0.2, shuffle=False)
 
         scaler = MinMaxScaler()
         train_data["Price"] = scaler.fit_transform(
@@ -71,72 +71,77 @@ if __name__ == "__main__":
             test_data["Price"].values.reshape(-1, 1)
         ).ravel()
 
-        def create_sequences(data, window_size=500):
+        def create_sequences(data, window_size):
             X, y = [], []
             for i in range(window_size, len(data)):
                 X.append(data[i - window_size : i])
                 y.append(data[i])
             return np.array(X), np.array(y)
 
-        X_train, y_train = create_sequences(train_data["Price"].values)
-        X_test, y_test = create_sequences(test_data["Price"].values)
+        window_size = len(test_data) // 10
+        X_train, y_train = create_sequences(
+            train_data["Price"].values, window_size=window_size
+        )
+        X_test, y_test = create_sequences(
+            test_data["Price"].values, window_size=window_size
+        )
 
         models = []
 
-        # # MLP
-        # mlp_model = Sequential()
-        # mlp_model.add(Dense(128, input_dim=X_train.shape[1], activation="relu"))
-        # mlp_model.add(Dense(32, activation="relu"))
-        # mlp_model.add(Dense(1))
-        # mlp_model.compile(optimizer="adam", loss="mse")
-        # mlp_model.fit(X_train, y_train, epochs=50, batch_size=32)
-        # models.append(("MLP", mlp_model))
+        # MLP
+        mlp_model = Sequential()
+        mlp_model.add(Dense(128, input_dim=X_train.shape[1], activation="relu"))
+        mlp_model.add(Dense(32, activation="relu"))
+        mlp_model.add(Dense(1))
+        mlp_model.compile(optimizer="adam", loss="mse")
+        mlp_model.fit(X_train, y_train, epochs=50, batch_size=32)
+        models.append(("MLP", mlp_model))
 
-        # # GRU
-        # gru_model = Sequential()
-        # gru_model.add(GRU(128, input_shape=(X_train.shape[1], 1)))
-        # gru_model.add(Dense(1))
-        # gru_model.compile(optimizer="adam", loss="mse")
-        # gru_model.fit(X_train, y_train, epochs=50, batch_size=32)
-        # models.append(("GRU", gru_model))
+        # GRU
+        gru_model = Sequential()
+        gru_model.add(GRU(128, input_shape=(X_train.shape[1], 1)))
+        gru_model.add(Dense(1))
+        gru_model.compile(optimizer="adam", loss="mse")
+        gru_model.fit(X_train, y_train, epochs=50, batch_size=32)
+        models.append(("GRU", gru_model))
 
-        # # LSTM
-        # lstm_model = Sequential()
-        # lstm_model.add(LSTM(128, input_shape=(X_train.shape[1], 1)))
-        # lstm_model.add(Dense(1))
-        # lstm_model.compile(optimizer="adam", loss="mse")
-        # lstm_model.fit(X_train, y_train, epochs=50, batch_size=32)
-        # models.append(("LSTM", lstm_model))
+        # LSTM
+        lstm_model = Sequential()
+        lstm_model.add(LSTM(128, input_shape=(X_train.shape[1], 1)))
+        lstm_model.add(Dense(1))
+        lstm_model.compile(optimizer="adam", loss="mse")
+        lstm_model.fit(X_train, y_train, epochs=50, batch_size=32)
+        models.append(("LSTM", lstm_model))
 
-        # # RNN
-        # rnn_model = Sequential()
-        # rnn_model.add(SimpleRNN(128, input_shape=(X_train.shape[1], 1)))
-        # rnn_model.add(Dense(1))
-        # rnn_model.compile(optimizer="adam", loss="mse")
-        # rnn_model.fit(X_train, y_train, epochs=50, batch_size=32)
-        # models.append(("RNN", rnn_model))
+        # RNN
+        rnn_model = Sequential()
+        rnn_model.add(SimpleRNN(128, input_shape=(X_train.shape[1], 1)))
+        rnn_model.add(Dense(1))
+        rnn_model.compile(optimizer="adam", loss="mse")
+        rnn_model.fit(X_train, y_train, epochs=50, batch_size=32)
+        models.append(("RNN", rnn_model))
 
-        # # XGBoost
-        # xgb_model = XGBRegressor()
-        # xgb_model.fit(X_train, y_train)
-        # models.append(("XGBoost", xgb_model))
+        # XGBoost
+        xgb_model = XGBRegressor()
+        xgb_model.fit(X_train, y_train)
+        models.append(("XGBoost", xgb_model))
 
-        # # LinearRegression
-        # linear_model = LinearRegression()
-        # linear_model.fit(X_train, y_train)
-        # models.append(("LinearRegression", linear_model))
+        # LinearRegression
+        linear_model = LinearRegression()
+        linear_model.fit(X_train, y_train)
+        models.append(("LinearRegression", linear_model))
 
-        # # SVR
-        # svr_model = SVR(kernel="rbf", gamma="scale", C=1.0, epsilon=0.1)
-        # svr_model.fit(X_train, y_train)
-        # models.append(("SVR", svr_model))
+        # SVR
+        svr_model = SVR(kernel="rbf", gamma="scale", C=1.0, epsilon=0.1)
+        svr_model.fit(X_train, y_train)
+        models.append(("SVR", svr_model))
 
         # ESN
-        # reservoir = Reservoir(reservoir_size=1000, lr=0.3, sr=0.8)
-        # ridge = Ridge(ridge=1e-7)
-        # esn = reservoir >> ridge
-        # esn.fit(X_train, y_train.reshape(-1, 1))
-        # models.append(("ESN", esn))
+        reservoir = Reservoir(units=5000, lr=0.3, sr=0.8)
+        ridge = Ridge(ridge=1e-7)
+        esn = reservoir >> ridge
+        esn.fit(X_train, y_train.reshape(-1, 1))
+        models.append(("ESN", esn))
 
         results = []
         for name, model in models:
